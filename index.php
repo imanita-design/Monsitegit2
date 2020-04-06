76b8b360f5bd553af2241c638273b6f7b405504b
imane <imane.mazouari@gmail.com>
2020-04-06 14:36:53 +0200

    premiere vesrion du projet

---
+++ index.php
@@ -0,0 +1,95 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap" rel="stylesheet">
+
+    <title>blog culinaire</title>
+  </head>
+  <body>
+  
+  <div class="container">
+
+    <h1 class="col-md-12 mx-auto display-4 text-center border border-dark mt-3">Bienvenue sur mon blog culinaire</h1>
+
+
+
+
+        <div class="row">
+                <div class="col-md-6">
+                        <img src="images/photo1.jpg" alt=""style="width:100%; height:40vh">
+                </div>
+
+                <div class="col-md-6">
+                         <p style="font-family: 'Pacifico', cursive;"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus recusandae numquam mollitia corporis optio perspiciatis cumque voluptas, laudantium provident, odit rem repellendus earum vitae atque commodi modi obcaecati Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio eos vitae ea placeat, provident culpa fuga ratione unde, mollitia assumenda temporibus facere dolores incidunt beatae iste deserunt officiis ipsam alias!</p>
+                </div>
+        </div>
+
+
+<hr class="col-md-6 mx-auto bd-success">
+
+        <div class="row">
+                
+                <div class="col-md-6">
+                        <p style="font-family: 'Pacifico', cursive;"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus recusandae numquam mollitia corporis optio perspiciatis cumque voluptas, laudantium provident, odit rem repellendus earum vitae atque commodi modi obcaecati Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio eos vitae ea placeat, provident culpa fuga ratione unde, mollitia assumenda temporibus facere dolores incidunt beatae iste deserunt officiis ipsam alias!</p>
+                </div>
+
+                <div class="col-md-6">
+                        <img src="images/photo2.jpg" alt=""style="width:100%; height:40vh">
+                </div>
+        </div>
+
+<hr class="col-md-6 mx-auto bd-success">
+
+        <div class="row">
+                
+                <div class="col-md-12">
+                         <p style="font-family: 'Pacifico', cursive;"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus recusandae numquam mollitia corporis optio perspiciatis cumque voluptas, laudantium provident, odit rem repellendus earum vitae atque commodi modi obcaecati Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio eos vitae ea placeat, provident culpa fuga ratione unde, mollitia assumenda temporibus facere dolores incidunt beatae iste deserunt officiis ipsam alias!</p>
+                </div>
+
+                <div class="col-md-12">
+                        <img src="images/photo3.jpg" alt="" style="width:100%; height:70vh">
+                </div>
+        </div>
+
+
+
+
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
